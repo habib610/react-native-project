7136c67209c06aa0a1fa6d7f8ff0eea3f0b70fa6
habib610 <habib54562@gmail.com>
2021-04-06 22:14:24 +0600

    initial-starting

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {  ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [names, setName]= useState([
+    {name: "Messi", id: 1 },
+    {name: "Neymar", id: 2 },
+    {name: "Ronaldo", id: 3 },
+    {name: "Persi", id: 4 },
+    {name: "Romero", id: 5 },
+    {name: "Ceaser", id: 6 },
+    {name: "MbAppe", id: 7 },
+    {name: "Riyaz", id: 8 },
+    {name: "Shabnur", id: 9 },
+  ])
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  <Text style={styles.boldText}>My Players List</Text>
+
+<ScrollView>
+    <View>
+      {
+        names.map(item=> <Text style={styles.name} key={item.id}>{item.name}</Text>)
+      }
+    </View>
+    </ScrollView>
+
+   
+
+
+     
+
+      <StatusBar style="auto"  />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    justifyContent: "center",
+    marginTop: 20,
+    alignItems: 'center'
   },
+  input: {
+    borderWidth: 1,
+    borderColor: "blue",
+    padding: 8,
+    margin: 10,
+    width: 300,
+  },
+  boldText: {
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center"
+  },
+  name: {
+    backgroundColor: "pink",
+    margin: 10,
+    padding: 20,
+    width: 300,
+    fontSize: 50,
+    fontWeight: "bold"
+  }
+ 
+
+
+
+
 });
